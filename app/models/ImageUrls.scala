9dc91ec2890ef432827306a4f3beae8d2b3a2259
parth262 <parthrpandya98@gmail.com>
2019-06-15 18:21:58 +0530

    Handling future with onComplete.

--- app/models/ImageUrls.scala
+++ app/models/ImageUrls.scala
@@ -7,6 +7,7 @@ import play.api.Logger
 import play.api.libs.ws._
 
 import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 class ImageUrls(ws: WSClient, implicit val ec: ExecutionContext, imgurConfig: Map[String, String]) {
   private val logger = Logger.logger
@@ -23,15 +24,15 @@ class ImageUrls(ws: WSClient, implicit val ec: ExecutionContext, imgurConfig: Ma
   private def processImage(url: String): Unit = {
     logger.info("Downloading image in memory")
     val request = ws.url(url)
-    request.get().map(response => {
-      logger.info("Image successfully downloaded")
-      logger.info("Uploading image to imgur")
-      response.status match {
-        case 200 => uploadImageToImgur(response.bodyAsBytes.toArray)
-        case status => logger.error(s"Error fetching image from url $url with status $status")
-      }
-    }).recover {
-      case _: Exception => logger.error(s"Error fetching image from $url")
+    request.get().onComplete {
+      case Success(response) =>
+        logger.info("Image successfully downloaded")
+        logger.info("Uploading image to imgur")
+        response.status match {
+          case 200 => uploadImageToImgur(response.bodyAsBytes.toArray)
+          case status => logger.error(s"Error fetching image from url $url with status $status")
+        }
+      case Failure(_) => logger.error(s"Error fetching image from $url")
     }
   }
 
@@ -40,13 +41,13 @@ class ImageUrls(ws: WSClient, implicit val ec: ExecutionContext, imgurConfig: Ma
       .addHttpHeaders("Authorization" -> s"Client-ID ${imgurConfig("id")}",
         "Content-Type" -> "application/x-www-form-urlencoded")
       .post(Map("image" -> new String(Base64.getEncoder.encode(imageBytes))))
-    futureResponse.map(response => {
-      response.status match {
-        case 200 => logger.debug(response.json.toString())
-        case status => logger.error(s"Error uploading to imgur with status $status")
-      }
-    }).recover {
-      case e: Exception => logger.error(s"Error => $e")
+    futureResponse.onComplete {
+      case Success(response) =>
+        response.status match {
+          case 200 => logger.debug(response.json.toString())
+          case status => logger.error(s"Error uploading to imgur with status $status")
+        }
+      case Failure(error) => logger.error(s"Error => $error")
     }
   }
 
